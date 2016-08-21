5e22ea5203818a35e84fae575bc89a0380eb6ad5
Avidor Turkewitz <avi.turkewitz@flatironschool.com>
2016-08-21 16:14:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array) {
+  array.forEach((elem, index, array) => {
+    console.log(`${index}: ${elem}`);
+  });
+}
+
+function iterate(callback) {
+  var newArray = ['empty','array'];
+  callback(newArray);
+  return newArray;
+}
+
+function doToArray(array, callback){
+  array.forEach(callback);
+}
